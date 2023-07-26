6f06344703a1b0e63c0e4f159bbc6e7e7373e3aa
Michael Pace <michael.g.pace@gmail.com>
2023-07-25 23:06:29 -0500

    file structure matches lab exactly, most lintr errors fixed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 require('dotenv').config();
-const app = require('./src/server.js');
+// const app = require('./src/server.js');
 const { db } = require('./src/auth/models');
-// const server = require('./src/server.js');
+const server = require('./src/server.js');
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 db.sync().then(() => {
   server.start(PORT);
